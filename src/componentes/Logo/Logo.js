67cff2f7e07d9ad2dceafb1f3bcfa3cc2437ad13
Kevin Ramos Dos Santos De Oliveira <kevram52@outlook.com>
2021-03-26 22:10:26 -0300

    logo centralizado

--- src/componentes/Logo/Logo.js
+++ src/componentes/Logo/Logo.js
@@ -5,10 +5,10 @@ import './Logo.css';
 
 const Logo = () => {
     return (
-        <div className = 'ma4 mt0'>
+        <div className = 'ma4 mt0 centralizado'>
             <Tilt className = "Tilt br2 shadow-2" options = {{ max: 50 }} style = {{ height: 150, width: 150 }}>
                 <div className = "Tilt-inner pa3"> 
-                    <img className = "center" style = {{ paddingTop: '5px' }} src = { logo } alt = "imagem"/> 
+                    <img style = {{ paddingTop: '5px' }} src = { logo } alt = "imagem"/> 
                 </div>
             </Tilt>
         </div>
